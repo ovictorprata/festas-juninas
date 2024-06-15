1fc13149676c7f20c08281c2cd14c37dcaefd62b
pratinha <dharllox@gmail.com>
2024-06-15 20:30:44 -0300

    arruma footer e header

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
       async function carregarFestas() {
         try {
-          const response = await fetch('festas.json'); // Carrega o arquivo festas.json
-          const festas = await response.json(); // Converte a resposta para JSON
-          mostrarFestas(festas); // Exibe as festas na página
+          const response = await fetch('festas.json'); 
+          const festas = await response.json(); 
+          mostrarFestas(festas); 
         } catch (error) {
           console.error('Erro ao carregar festas.json:', error);
         }
@@ -13,12 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
       function mostrarFestas(listaFestas) {
         listaFestas.forEach(festa => {
+        const partesData = festa.data.split('-');
+        const ano = partesData[0];
+        const mes = partesData[1];
+        const dia = partesData[2];
+        
+        const dataFormatada = `${dia}/${mes}/${ano}`;
+
           const div = document.createElement('div');
           div.classList.add('col-lg-6');
           div.innerHTML = `
               <div class="festa">
                   <h2 class="card-title">${festa.local}</h2>
-                  <p><strong>Data:</strong> ${festa.data}</p>
+                  <p><strong>Data:</strong> ${dataFormatada}</p>
                   <p><strong>⏰</strong> ${festa.horario}</p>
                   <p><strong>Preço:</strong> ${festa.preco}</p>
                   <p><strong>Endereço:</strong> ${festa.endereco}</p>
